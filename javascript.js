bb56bdb0ad3a75a695f150ad328db3cb25fc13c8
CondurachiCristian <107665724+CondurachiCristian@users.noreply.github.com>
2024-10-03 15:46:29 +0300

    Wrote code for a round of play

--- javascript.js
+++ javascript.js
@@ -1,18 +1,45 @@
-
-function getComputerChoice(){ 
-     Math.floor(Math.random()*3)
-     if (Math.floor(Math.random()*3) == 0) {
-        return 'I choose rock'
-     } else if (Math.floor(Math.random()*3) == 1) {
-        return 'I choose paper'
-     } else {
-        return 'I choose scissors'
-     }
+function getComputerChoice() {
+   let choice = Math.floor(Math.random()*3);
+   if (choice == 0) {
+      return choice
+   } else if (choice == 1) {
+      return choice
+   } else {
+      return choice
+   }
 };
 
-function getHumanChoice (){
-    return prompt("Rock, Paper or Scissors?")
+function getHumanChoice() {
+   let answer = prompt("Rock, Paper or Scissors?");
+   answer = answer.toLowerCase()
+   if (answer == "rock") {
+      answer = 0
+   } else if (answer == "paper") {
+      answer = 1
+   } else answer = 2
+      return answer;
 }
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+   if (humanChoice > computerChoice) {
+      +humanScore
+      return "You win!"
+   } else if (humanChoice < computerChoice) {
+      +computerScore
+      return "You lose!"
+   } else {
+      return "You draw!"
+   }
+ };
+ 
+ const humanSelection = getHumanChoice();
+ const computerSelection = getComputerChoice();
+ 
+ playRound(humanSelection, computerSelection);
+
+ console.log(humanSelection, computerSelection)
+ console.log(playRound(humanSelection, computerSelection));
+ console.log(humanScore, computerScore)
